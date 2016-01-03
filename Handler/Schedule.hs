f41dafa00e13edfe786654b10ca5104ff7622c91
doilux <doilux0309@gmail.com>
2016-01-03 11:41:20 +0900

    Add Schedule Handler

---
+++ Handler/Schedule.hs
@@ -0,0 +1,9 @@
+module Handler.Schedule where
+
+import Import
+
+getScheduleR :: ScheduleId -> Handler Html
+getScheduleR scheduleId = error "Not yet implemented: getScheduleR"
+
+postScheduleR :: ScheduleId -> Handler Html
+postScheduleR scheduleId = error "Not yet implemented: postScheduleR"
---
+++ test/Handler/ScheduleSpec.hs
@@ -0,0 +1,14 @@
+module Handler.ScheduleSpec (spec) where
+
+import TestImport
+
+spec :: Spec
+spec = withApp $ do
+
+    describe "getScheduleR" $ do
+        error "Spec not implemented: getScheduleR"
+
+
+    describe "postScheduleR" $ do
+        error "Spec not implemented: postScheduleR"
+
